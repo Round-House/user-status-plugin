7d22ab33c7f8d4333a4476e09677be863ab722d0
William Parker <wiparker2020@gmail.com>
2022-08-17 22:42:33 -0400

    Minimal plugin implementation working.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const microservicesOptions = {
+  transport: Transport.REDIS,
+  options: {
+    //url: 'redis://redis:6379',
+    host: 'redis',
+    port: 6379,
+    /*
+    url: `redis://${process.env.REDIS_HOST}:${parseInt(
+      process.env.REDIS_PORT,
+    )}`,
+    */
+  },
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microservicesOptions,
+  );
+  app.listen();
 }
 bootstrap();
